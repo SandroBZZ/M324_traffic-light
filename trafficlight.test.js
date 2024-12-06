a32d808dd3622f5a07df009778d86492a8a56f92
SandroBZZ <96289247+SandroBZZ@users.noreply.github.com>
2024-12-06 11:08:44 +0100

    Update trafficlight.test.js

--- trafficlight.test.js
+++ trafficlight.test.js
@@ -3,4 +3,13 @@ describe('test suite traffic light', () =>{
     it('case red light', ()=>{
         expect(app.trafficLight("red")).toBe("DON'T WALK!");
     })
+    it('case red light', ()=>{
+        expect(app.trafficLight("orange")).toBe("ATTENTION!");
+    })
+    it('case red light', ()=>{
+        expect(app.trafficLight("green")).toBe("WALK!");
+    })
+    it('case red light', ()=>{
+        expect(app.trafficLight(undefined)).toBe("OUT OF ORDER!");
+    })
 })
